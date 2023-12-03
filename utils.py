d6252f32a55f3044206c47bac1fb82fab287f363
Daniel Kabata <youitik@hotmail.com>
2023-12-02 22:48:59 -0300

    - commit inicial

---
+++ utils.py
@@ -0,0 +1,24 @@
+import pandas as pd
+import numpy as np
+
+
+
+print("oi")
+print("oi 2")
+
+
+a = "oi"
+
+print(a)
+
+pd.read_csv
+
+
+help(pd)
+
+
+import yfinance as yf
+pip install yfinance
+
+
+
